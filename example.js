8900d8dd5abf984ce2a532500564fd95cf543f3a
shirotamaki <shiro.tamaki63@gmail.com>
2023-02-05 21:24:10 +0900

    example.jsファイルを追加

---
+++ example.js
@@ -0,0 +1,17 @@
+export default function() {
+  return (
+    <div>
+      Enter
+    </div>
+  );
+}
+
+const example = () => {
+  return (
+    <div>
+      Enter
+    </div>
+  );
+}
+
+export default example;
