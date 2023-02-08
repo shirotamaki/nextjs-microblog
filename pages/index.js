97efa9b2b730f653308784d5c80b46d243f40027
shirotamaki <shiro.tamaki63@gmail.com>
2023-02-08 20:53:40 +0900

    Homeモジュールを追加、CSSをあてる

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,74 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-import Link from 'next/link'
-import Layout from '../components/Layout'
-import utilStyle from "../styles/utils.module.css"
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
+import Link from "next/link";
+import Layout from "../components/Layout";
+import utilStyle from "../styles/utils.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <Layout>
       <section className={utilStyle.headingMd}>
-        <p>ロックンロール・プログラマーです</p>
+        <p>わん、わん、わ、わーーーん</p>
       </section>
 
-      <section>
+      <section className={utilStyle.headingMd}>
         <h2>📝メモタイトル</h2>
-        <div>
+        <div className={styles.grid}>
           <article>
             <Link href="/">
-              <img src="/images/thumbnail01.jpg" alt="" />
+              <img
+                src="/images/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+              />
             </Link>
             <Link href="/" legacyBehavior>
-              <a>タイトルだよ</a>
+              <a className={utilStyle.boldText}>タイトルだよ</a>
             </Link>
             <br />
-            <samll>2023年2月</samll>
+            <samll className={utilStyle.lightText}>2023年2月1日</samll>
+          </article>
+          <article>
+            <Link href="/">
+              <img
+                src="/images/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link href="/" legacyBehavior>
+              <a className={utilStyle.boldText}>タイトルだよ</a>
+            </Link>
+            <br />
+            <samll className={utilStyle.lightText}>2023年2月1日</samll>
+          </article>
+          <article>
+            <Link href="/">
+              <img
+                src="/images/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link href="/" legacyBehavior>
+              <a className={utilStyle.boldText}>タイトルだよ</a>
+            </Link>
+            <br />
+            <samll className={utilStyle.lightText}>2023年2月1日</samll>
+          </article>
+          <article>
+            <Link href="/">
+              <img
+                src="/images/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link href="/" legacyBehavior>
+              <a className={utilStyle.boldText}>タイトルだよ</a>
+            </Link>
+            <br />
+            <samll className={utilStyle.lightText}>2023年2月1日</samll>
           </article>
         </div>
       </section>
